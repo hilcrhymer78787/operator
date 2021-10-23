f08d218639669d0df9273b06a41a361ca0a7322f
今野龍之介 <hilcrhymer78787@gmail.com>
2021-10-24 07:00:44 +0900

    test

--- nuxt/pages/admin/user.vue
+++ nuxt/pages/admin/user.vue
@@ -11,37 +11,19 @@
             <li v-for="(user,userIndex) in loginInfo.users" :key="user.id">
                 <v-list-item class="pl-2 pr-0" style="height:60px;overflow:hidden;">
                     <v-list-item-avatar>
-                        <v-img :src="loginInfo.user_img" aspect-ratio="1" class="rounded-circle main_img"></v-img>
+                        <v-img :src="user.user_img" aspect-ratio="1" class="rounded-circle main_img"></v-img>
                     </v-list-item-avatar>
                     <v-list-item-content>
                         <v-list-item-title>{{user.name}}</v-list-item-title>
+                        <v-list-item-subtitle style="font-size:12px;">
+                            <span>給与:7000</span>、
+                        </v-list-item-subtitle>
                     </v-list-item-content>
                     <v-btn dark color="sub" class="pa-0" style="margin-right:6px;">edit</v-btn>
                 </v-list-item>
                 <v-divider v-if="userIndex + 1 != loginInfo.users.length"></v-divider>
             </li>
         </ul>
-
-        <!-- <v-simple-table>
-            <thead>
-                <tr>
-                    <th class="text-left">id</th>
-                    <th class="text-left">Name</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="user in loginInfo.users" :key="user.id">
-                    <td>{{ user.id }}</td>
-                    <td>{{ user.name }}</td>
-                    <td>
-                        <div class="d-flex justify-end">
-                            <v-btn dark @click="dialogOpen(user.id,user.name)" color="sub" class="pa-0" style="margin-right:6px;">edit</v-btn>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </v-simple-table> -->
     </v-card>
 </template>
 
