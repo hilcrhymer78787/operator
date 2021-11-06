020e9ed38b8b9be5d01abb9f12f6154a0f5a079b
今野龍之介 <hilcrhymer78787@gmail.com>
2021-11-06 18:07:02 +0900

    リダイレクト処理

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,13 +1,14 @@
 <template>
-  
+    <div></div>
 </template>
-
 <script>
 export default {
-
+    middleware({ redirect }) {
+        redirect(
+            `/member/?year=${new Date().getFullYear()}&month=${
+                new Date().getMonth() + 1
+            }`
+        )
+    },
 }
 </script>
-
-<style>
-
-</style>
