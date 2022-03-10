3f6a4592f5a1afc73deccf1f9ad2c3ce38188b9d
今野龍之介 <hilcrhymer78787@gmail.com>
2022-03-10 14:56:02 +0900

    test

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -12,8 +12,7 @@ export default {
         title: 'operator',
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            // { name: 'viewport', content: 'viewport-fit=cover' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
             { hid: 'description', name: 'description', content: '' },
             { name: 'format-detection', content: 'telephone=no' },
             { name: 'apple-mobile-web-app-capable', content: 'yes' },
