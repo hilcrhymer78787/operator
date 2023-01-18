ec2db9bdc04a518441889b27cded475a6f20afbf
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2023-01-18 10:00:07 +0900

    error log

--- nuxt/plugins/axios.js
+++ nuxt/plugins/axios.js
@@ -3,4 +3,12 @@ export default function ({ $axios, $cookies }) {
         config.headers.token = $cookies.get("token")
         return config
     })
+    $axios.onError((error) => {
+        if (error.response) {
+            console.error(error.response);
+            if (error.response.status == 429) {
+                alert('一定時間にアクセスが集中したため、しばらくアクセスできません')
+            }
+        }
+    })
 }
