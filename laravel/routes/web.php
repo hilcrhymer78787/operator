2ab78dc707ce56cb26393bb47576bce069a001ce
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-06-01 11:07:19 +0900

    fix

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LineMessengerController;
+use App\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // LINE メッセージ受信（Webhook）
-Route::post('/line/webhook', [LineMessengerController::class, 'webhook']);
+Route::post('/line/webhook', [LineMessengerController::class, 'webhook'])
+    ->withoutMiddleware([VerifyCsrfToken::class]);
