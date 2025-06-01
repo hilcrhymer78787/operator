fc9ee3ca05c7d03890d77e3be4bf68eb31d205d9
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-06-01 09:29:58 +0900

    fix

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// LINE メッセージ受信
+Route::post('/line/webhook', 'LineMessengerController@webhook');
