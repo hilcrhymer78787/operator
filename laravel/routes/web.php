4dc04a3a99b43e17b1a4b446cae57a0b7245f6f7
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-06-01 09:31:56 +0900

    fix

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LineMessengerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// LINE メッセージ受信
-Route::post('/line/webhook', 'LineMessengerController@webhook');
+// LINE メッセージ受信（Webhook）
+Route::post('/line/webhook', [LineMessengerController::class, 'webhook']);
